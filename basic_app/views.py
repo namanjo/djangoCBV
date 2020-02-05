e057a444bc1306a0b892773d9b181f9fc5c80268
Naman Joshi <naman14joshi@gmail.com>
2020-02-05 19:56:21 +0530

    Implemented Listview & DetailView.

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
-from django.http import HttpResponse
+from django.views.generic import (View, TemplateView,
+                                ListView, DetailView,
+                                CreateView, UpdateView, DeleteView)
+from basic_app import models
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class SchoolListView(ListView):
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    model = models.School
+    context_object_name = 'school_detail'
+    template_name = 'basic_app/school_detail.html'
--- djangoCBV/urls.py
+++ djangoCBV/urls.py
@@ -19,5 +19,6 @@ from basic_app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('basic_app/', include('basic_app.urls', namespace= 'basic_app')),
     path('admin/', admin.site.urls),
 ]
